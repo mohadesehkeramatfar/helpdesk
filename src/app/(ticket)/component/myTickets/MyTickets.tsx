e895f498e4fd538c661439bdda1ab63b2d3507a2
Moahdeseh <mohadesehkeramatfar67@gmail.com>
2024-08-13 11:06:05 +0330

    responsiving my ticket

--- src/app/(ticket)/component/myTickets/MyTickets.tsx
+++ src/app/(ticket)/component/myTickets/MyTickets.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Card,
   Divider,
   Space,
   Spin,
@@ -17,40 +18,46 @@ import { TbListDetails } from 'react-icons/tb';
 import { useRouter } from 'next/navigation';
 import { AiOutlineRise } from 'react-icons/ai';
 import { IoCheckmarkOutline } from 'react-icons/io5';
-import { FcProcess } from 'react-icons/fc';
 import Title from 'antd/es/typography/Title';
+import { PiArrowsCounterClockwiseThin } from 'react-icons/pi';
+import useResponsive from '@/lib/hook/useResponsive';
 const { Text } = Typography;
 
-const statisticData = [
-  {
-    color: 'default',
-    icon: <AiOutlineRise size={40} />,
-    title: 'کل',
-    value: 0,
-  },
-  {
-    color: 'processing',
-    icon: <FcProcess size={40} color="#1677ff" />,
-    title: 'در حال بررسی',
-    value: 0,
-  },
-  {
-    color: 'success',
-    icon: <IoCheckmarkOutline size={40} />,
-    title: 'بسته',
-    value: 0,
-  },
-];
 const MyTickets = () => {
   const router = useRouter();
   const { data: getUnitTicketList, isLoading: getUnitTicketListLoading } =
     useGetUnitTicketList();
+  const { isMobile } = useResponsive();
   const [ticketListPagination, setTicketListPagination] = useState({
     total: 0,
     current: 1,
     pageSize: 50,
   });
-
+  const statisticData = [
+    {
+      color: '#9e9e9e',
+      icon: <AiOutlineRise size={isMobile ? 20 : 40} />,
+      title: 'کل',
+      value: 0,
+    },
+    {
+      color: '#009bff',
+      icon: (
+        <PiArrowsCounterClockwiseThin
+          size={isMobile ? 20 : 40}
+          color="#1677ff"
+        />
+      ),
+      title: 'در حال بررسی',
+      value: 0,
+    },
+    {
+      color: '#5cb85c',
+      icon: <IoCheckmarkOutline size={isMobile ? 20 : 40} />,
+      title: 'بسته',
+      value: 0,
+    },
+  ];
   const handlePagination = (page: { current: number }) => {
     const { current } = page;
     setTicketListPagination({ ...ticketListPagination, current });
@@ -73,7 +80,7 @@ const MyTickets = () => {
         return (
           <Space direction="vertical">
             {' '}
-            <Text>{categories[0]?.parent.name}</Text>
+            <Text>{categories[0]?.parent?.name}</Text>
             <Text type="secondary">{categories[0]?.name}</Text>
           </Space>
         );
@@ -125,30 +132,54 @@ const MyTickets = () => {
 
   return (
     <div className={`${style.my_ticket_container}`}>
-      <div>
-        <Title level={3}>درخواست‌های من</Title>
+      <div className={`${style.title_container}`}>
+        <Title level={4}>تیکت‌های من</Title>
         <Divider style={{ paddingBottom: 0, marginBottom: 0 }} />
       </div>
       <div className={`${style.statistics_container}`}>
         {statisticData.map((item, index) => (
-          <div key={index} className={`${style.statistic}`}>
-            <Tag color={item.color} className={style.tag}>
-              {item.icon}
-            </Tag>
-            <Statistic title={item.title} value={item.value} />
-          </div>
+          // <div key={index} className={`${style.statistic}`}>
+          <Card key={index} bordered={false}>
+            <Statistic
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+              title={
+                <span style={{ color: item.color, fontWeight: 700 }}>
+                  {item.title}
+                </span>
+              }
+              value={item.value}
+              valueStyle={{ color: item.color }}
+              // prefix={item.icon}
+            />
+          </Card>
         ))}
       </div>
-
-      <Table
-        dataSource={unitTicketList}
-        columns={unitTicketListColumns}
-        pagination={ticketListPagination}
-        onChange={handlePagination}
-        rowClassName={() => globalStyle.customRowHeight}
-      />
+      <div className={`${style.table_container}`}>
+        <Table
+          onRow={(record) => ({
+            onClick: () => router.push(`/my-ticket/${record.id}`),
+          })}
+          dataSource={unitTicketList}
+          columns={unitTicketListColumns}
+          pagination={ticketListPagination}
+          onChange={handlePagination}
+          rowClassName={() => globalStyle.customRowHeight}
+          scroll={{ x: 'max-content' }}
+        />
+      </div>
     </div>
   );
 };
 
 export default MyTickets;
+// </div>
+// <div key={index} className={`${style.statistic}`}>
+//   {/* <Tag color={item.color} className={style.tag}> */}
+//   {isMobile ? null : item.icon}
+//   {/* </Tag> */}
+//   <Statistic title={item.title} value={item.value} />
+// </div>
