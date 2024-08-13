e187f4225cd98d9a265991e36477d8a0e9f7aa1d
Moahdeseh <mohadesehkeramatfar67@gmail.com>
2024-08-13 08:47:26 +0330

    update base url

--- src/lib/services/baseURL.ts
+++ src/lib/services/baseURL.ts
@@ -3,7 +3,7 @@ import { getRefreshToken, getToken, setToken } from '../token';
 import { API_ENDPOINTS } from './endpoints';
 
 export const client = axios.create({
-  baseURL: 'http://172.16.203.26:8080',
+  baseURL: 'http://172.16.203.64:8080',
 });
 
 const renewToken = async () => {
