974db0bb777b2213d1405324f596a000c9e961a5
Moahdeseh <mohadesehkeramatfar67@gmail.com>
2024-08-13 08:47:38 +0330

    add alert message

--- src/lib/alertMessage.ts
+++ src/lib/alertMessage.ts
@@ -1,5 +1,7 @@
 export const generalMessage =
   'مشکلی پیش آمده است، لطفا با پشتیبانی تماس بگیرید';
 
-export const unitSuccessfullRegister = 'ثبت نام شما با موفقیت انجام شد';
+export const successfulUnitRegister = 'ثبت نام شما با موفقیت انجام شد';
 export const wrongPassword = 'رمز وارد شده اشتباه میباشد';
+
+export const successfulTicketRegister = 'تیکت با موفقیت ثبت شد';
