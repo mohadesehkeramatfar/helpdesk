ee7f7a1d9f7b4365c09a557394ac46ddb371dff2
Moahdeseh <mohadesehkeramatfar67@gmail.com>
2024-08-13 14:55:58 +0330

    bug fix header

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,30 +1,35 @@
 'use client';
-import { Button, Tooltip } from 'antd';
+import { Button, Spin } from 'antd';
 import style from './header.module.scss';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { getCookies, removeCookies } from '@/lib/cookies';
 import { getStorage, removeStorage } from '@/lib/storage';
-import { GoSignIn } from 'react-icons/go';
-import { CiLogout } from 'react-icons/ci';
-import { useEffect, useState } from 'react';
 import globalStyle from '../../layout.module.scss';
 import useResponsive from '@/lib/hook/useResponsive';
+import {
+  contactUsPageRoute,
+  homePageRoute,
+  myTicketPageRoute,
+  sendTicketPageRoute,
+} from '@/lib/services/routes';
+import { LuUser } from 'react-icons/lu';
+import { LiaSignInAltSolid, LiaSignOutAltSolid } from 'react-icons/lia';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
   const router = useRouter();
-  const [userLable, setUserlable] = useState('ورود');
-  const user_info = getStorage('user_name' ?? '{}');
-  const { isDesktop, isMobile, isTablet } = useResponsive();
-
+  const [loading, setLoading] = useState(true);
+  const { isDesktop, isMobile } = useResponsive();
+  const [userInfo, setUserInfo] = useState('');
   const logoutHandler = () => {
     removeStorage('user_id');
     removeStorage('user_name');
     removeCookies('access');
     removeCookies('refresh');
-    setUserlable('ورود');
     router.push('/');
+    setLoading(true);
   };
   const handleSendTicket = (e) => {
     e.preventDefault();
@@ -35,55 +40,68 @@ const Header = () => {
   };
 
   useEffect(() => {
-    if (user_info) setUserlable(user_info);
-  }, [userLable, router]);
+    if (loading) {
+      setUserInfo(getStorage('user_name' ?? ''));
+      setLoading(false);
+    }
+  }, [loading]);
 
   return (
     <div className={`${style.header_container}`}>
-      {/* header_container */}
       <div className={`${globalStyle.container} ${style.header}`}>
         <Image
           className={`${style.logo}`}
-          width={120}
-          height={60}
+          width={isMobile ? 60 : 90}
+          height={isMobile ? 40 : 60}
           alt={'تی‌ویژه'}
           src="/logo/TV.png"
         />
         {isDesktop && (
           <ul className={`${style.list_menu}`}>
             <li>
-              <Link href="/">صفحه اصلی</Link>
+              <Link href={homePageRoute}>صفحه اصلی</Link>
             </li>
             <li>
-              <Link href="">تماس با ما</Link>
+              <Link href={contactUsPageRoute}>تماس با ما</Link>
             </li>
             <li>
-              <Link href={''} onClick={handleSendTicket}>
+              <Link href={sendTicketPageRoute} onClick={handleSendTicket}>
                 ارسال تیکت
               </Link>
             </li>
             <li>
-              <Link href={'/my-ticket'}>تیکت های من</Link>
+              <Link href={myTicketPageRoute}>تیکت های من</Link>
             </li>
           </ul>
         )}
 
         {getCookies('access') ? (
           <div className={`${style.auth_container}`}>
-            {' '}
-            <Button type="text">{userLable}</Button>
-            <Tooltip title="خروج">
-              {' '}
-              <Button type="text" onClick={logoutHandler}>
+            {loading ? (
+              <Spin />
+            ) : (
+              <>
                 {' '}
-                <CiLogout size={22} />
-              </Button>
-            </Tooltip>
+                <Button icon={<LuUser size={20} />} type="text">
+                  {userInfo}
+                </Button>
+                <Button
+                  onClick={logoutHandler}
+                  icon={<LiaSignOutAltSolid size={20} />}
+                  type="text"
+                >
+                  خروج
+                </Button>
+              </>
+            )}
           </div>
         ) : (
-          <Button type="default" onClick={() => router.push('/auth/signin')}>
-            {userLable}
-            <GoSignIn size={21} />
+          <Button
+            icon={<LiaSignInAltSolid size={20} />}
+            type="text"
+            onClick={() => router.push('/auth/signin')}
+          >
+            ورود
           </Button>
         )}
       </div>
