2639cf9137c0b357fb476a5b7128dad5a80fe845
Moahdeseh <mohadesehkeramatfar67@gmail.com>
2024-08-13 08:47:10 +0330

    add router general

---
+++ src/lib/services/routes.ts
@@ -0,0 +1,4 @@
+export const homePageRoute = '/';
+export const sendTicketPageRoute = '/send-ticket';
+export const myTicketPageRoute = '/my-ticket';
+export const contactUsPageRoute = '/contact-us';
