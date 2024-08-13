ef21a5fa604c24a340f07bf3fd220b1513668cf3
Moahdeseh <mohadesehkeramatfar67@gmail.com>
2024-08-13 08:51:01 +0330

    add router to bottom nav

--- src/app/_components/bottomNav/BottomNav.tsx
+++ src/app/_components/bottomNav/BottomNav.tsx
@@ -6,50 +6,64 @@ import { RiHome2Line } from 'react-icons/ri';
 import { VscSend } from 'react-icons/vsc';
 import style from './bottomNav.module.scss';
 import { IoListOutline } from 'react-icons/io5';
-const menuItem = [
-  {
-    key: 'home',
-    title: 'صفحه اصلی',
-    icon: <RiHome2Line size={22} />,
-    style: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-  },
-  {
-    key: 'send_ticket',
-    title: 'ارسال تیکت',
-    icon: <VscSend size={22} />,
-    style: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-  },
-  {
-    key: 'myTicket',
-    title: 'تیکت‌های من',
-    icon: <IoListOutline size={22} />,
-    style: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-  },
-  {
-    key: 'contactUs',
-    title: 'تماس با ما',
-    icon: <PiPhone size={22} />,
-    style: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-  },
-];
+import { usePathname, useRouter } from 'next/navigation';
+import {
+  contactUsPageRoute,
+  homePageRoute,
+  myTicketPageRoute,
+  sendTicketPageRoute,
+} from '@/lib/services/routes';
+
 const BottomNav = () => {
   const { isDesktop } = useResponsive();
+  const pathname = usePathname();
+  const router = useRouter();
+  const menuItem = [
+    {
+      key: 'home',
+      title: 'صفحه اصلی',
+      icon: <RiHome2Line size={22} />,
+      onclick: () => router.push(homePageRoute),
+      style: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+    },
+    {
+      key: 'send_ticket',
+      title: 'ارسال تیکت',
+      icon: <VscSend size={22} />,
+      onclick: () => router.push(sendTicketPageRoute),
+      style: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+    },
+    {
+      key: 'myTicket',
+      title: 'تیکت‌های من',
+      icon: <IoListOutline size={22} />,
+      onclick: () => router.push(myTicketPageRoute),
+      style: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+    },
+    {
+      key: 'contactUs',
+      title: 'تماس با ما',
+      icon: <PiPhone size={22} />,
+      onclick: () => router.push(contactUsPageRoute),
+      style: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+    },
+  ];
   return (
     !isDesktop && (
       <div className={`${style.container}`}>
@@ -57,6 +71,7 @@ const BottomNav = () => {
         <Menu
           mode="horizontal"
           className={`${style.menu}`}
+          // selectedKeys={[pathname]}
           style={{
             borderTop: '1px solid #f0f0f0',
             padding: '10px 0',
@@ -73,6 +88,7 @@ const BottomNav = () => {
               key={item.key}
               icon={item.icon}
               style={item.style}
+              onClick={item.onclick}
             >
               {item.title}
             </Menu.Item>
