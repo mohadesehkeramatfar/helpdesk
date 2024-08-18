7d7aa1bebc70111db1697c89c2178001c9496650
Moahdeseh <mohadesehkeramatfar67@gmail.com>
2024-08-18 13:02:29 +0330

    update variable and endpoint

--- src/lib/services/endpoints.ts
+++ src/lib/services/endpoints.ts
@@ -17,5 +17,7 @@ export const API_ENDPOINTS = {
   POST_UNIT_TICKET_POST_SUBMIT: '/ticket/unit_ticket_post_submit/',
   GET_UNIT_TICKET_LIST: '/ticket/unit_tickets_list/',
   GET_UNIT_TICKET_Detail: '/ticket/unit_ticket_detail/',
+  GET_UNIT_TICKET_POST_LIST: '/ticket/unit_ticket_ticket_posts_list/',
+  GET_TICKET_TIMELINE: '/ticket/ticket_timeline/',
   //
 };
