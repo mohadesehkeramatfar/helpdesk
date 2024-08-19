f7fe6d7925b954f8cbdf8e5ef2107e7b98c70d8b
Moahdeseh <mohadesehkeramatfar67@gmail.com>
2024-08-19 09:37:46 +0330

    order items form-update endpoint register and bug fix

--- src/app/auth/component/passwordSignin.tsx
+++ src/app/auth/component/passwordSignin.tsx
@@ -6,15 +6,16 @@ import { ToastComponent } from '@/app/_components/toast/toast';
 import { toast } from 'react-toastify';
 import { generalMessage, wrongPassword } from '@/lib/alertMessage';
 import { setRefreshToken, setToken } from '@/lib/token';
-import { setStorage } from '@/lib/storage';
 import { myTicketPageRoute, sendTicketPageRoute } from '@/lib/services/routes';
+import { useRef } from 'react';
 
 const PasswordSignin = () => {
   const router = useRouter();
-
+  const refPassword = useRef();
   const searchParams = useSearchParams();
   const { trigger: postUnitLogin } = usePostUnitLogin();
   const { trigger: getUserInfo } = useGetUserInfo();
+
   const phone = searchParams.get('phone');
   const sendTicket = searchParams.get('send-ticket');
   const myTicket = searchParams.get('my-ticket');
@@ -42,16 +43,16 @@ const PasswordSignin = () => {
       if (error?.response?.data[0] == 'Unit not found') {
         toast.error(wrongPassword);
       } else {
-        toast.error(generalMessage);
+        try {
+          const { data: getUserInfoResponsed } = await getUserInfo();
+          const userName =
+            getUserInfoResponsed.first_name +
+            ' ' +
+            getUserInfoResponsed.last_name;
+        } catch (error) {
+          toast.error(generalMessage);
+        }
       }
-    }
-    try {
-      const { data: getUserInfoResponsed } = await getUserInfo();
-      const userName =
-        getUserInfoResponsed.first_name + ' ' + getUserInfoResponsed.last_name;
-      setStorage('user_name', userName);
-    } catch (error) {
-      toast.error(generalMessage);
     }
   };
   const formItems = [
@@ -59,6 +60,7 @@ const PasswordSignin = () => {
       name: 'password',
       label: 'رمز عبور',
       component: Input.Password,
+      ref: refPassword,
       rules: [{ required: true, message: 'رمز را وارد کنید' }],
     },
   ];
